d062e3c1ce71d3822691c5fc28fc5103d1459b01
winigajordan <winigajordan@gmail.com>
2023-01-15 15:00:49 +0000

    initialisation du projet

---
+++ src/test/java/io/winigajordan/SpringDataJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.winigajordan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
